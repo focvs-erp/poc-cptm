9873ea4d1c301a9c52ecf47a8afc1522fa8adcc2
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-04-26 18:28:08 -0300

    habilitar model para o aplicativo cptm-customs

--- cptm_custom/models/models.py
+++ cptm_custom/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class cptm_custom(models.Model):
-#     _name = 'cptm_custom.cptm_custom'
-#     _description = 'cptm_custom.cptm_custom'
+class cptm_custom(models.Model):
+    _name = 'cptm_custom.cptm_custom'
+    _description = 'cptm_custom.cptm_custom'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100

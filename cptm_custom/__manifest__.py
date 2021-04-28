cc02e98fa80e4d657d89201f8b628466aad7eb21
DougalsLop <douglas.silva@ax4b.com>
2021-04-28 14:48:47 +0000

    commit teste

--- cptm_custom/__manifest__.py
+++ cptm_custom/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "AX4B",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
+    #  Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',

9cbaf642374477e205d9f867df3bb67c8ffdf5b3
DouglasLop <douglas.silva@ax4b.com>
2021-04-28 20:30:51 +0000

    criação do aplicativo cptm_inventory

--- cptm_inventory/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ cptm_inventory/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "cptm_custom",
+    'name': "cptm_inventory",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,10 +10,10 @@
         Long description of module's purpose
     """,
 
-    'author': "AX4B",
+    'author': "My Company",
     'website': "http://www.yourcompany.com",
 
-    #  Categories can be used to filter modules in modules listing
+    # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
@@ -24,12 +24,9 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/baixa_estoque.xml',
         'views/templates.xml',
-        'views/cotacao_compra.xml',
-        
     ],
     # only loaded in demonstration mode
     'demo': [

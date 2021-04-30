641c6e2362ae0a50dc9ee01c3463ee5b5d21b551
Anderson Peruci <anderson.peruci@ax4b.com>
2021-04-30 18:37:06 -0300

    Adicionando número sequencial ao id referencial da model

--- ax4b_inventory/models/baixa_estoque.py
+++ ax4b_inventory/models/baixa_estoque.py
@@ -7,19 +7,7 @@ class BaixaEstoque(models.Model):
     _name = 'stock.baixa_estoque'
     _description = 'stock.baixa_estoque'
 
-    # numero_requisicao = fields.Text(string = "Número da Requisição")
-    # numero_requisicao = fields.Char('My Sequence', readonly=True)
-
-    numero_requisicao = fields.Char('Número da Requisição')
-
-    # on create method
-    @api.model
-    def create(self, vals):
-        obj = super(BaixaEstoque, self).create(vals)
-        number = self.env['ir.sequence'].get('x_baixa_estoque')
-        obj.write({'numero_requisicao': number})
-        return obj
-
+    numero_requisicao = fields.Char('Número da Requisição', readonly=True)
     tipo_requisicao = fields.Selection([('Consumo', 'Consumo'),('Ativo', 'Ativo'),('Insumo', 'Insumo')], string = "Tipo da Requisição")
     dt_emissao = fields.Date(string = 'Data de Emissão')
     dt_lancamento = fields.Date(string = 'Data de Lançamento')
@@ -33,12 +21,21 @@ class BaixaEstoque(models.Model):
         ("PRONTO", "PRONTO"),
         ("CONCLUIDO", "CONCLUIDO"),
     ], default = "PROVISÓRIO")
-    
-
     tabela_requisicao = fields.Many2one('x_requisicoes_de_compr', string = "Requisição")
     nome_da_requisicao_related = fields.Char(related='tabela_requisicao.x_studio_nome_da_requisio', string="Nome da Requisição")
     produtos_da_requisicao_related = fields.One2many(related='tabela_requisicao.x_studio_one2many_field_jwTT5', string="Produto da Requisição")  
         
+
+    # on create method
+    @api.model
+    def create(self, vals):
+        obj = super(BaixaEstoque, self).create(vals)
+        number = self.env['ir.sequence'].get('x_baixa_estoque')
+        obj.write({'numero_requisicao': number})
+        return obj
+
+
+
 #   cd_produto = fields.One2Many(string= "Código do Produto")
 #    valor_total = fields.Float(string = "Valor Total", compute="_valor_total", store=True)
 #     valor_unitario = fields.Float(string = "Valor Unitario")

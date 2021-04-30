b759a48e4003f9b214a291941cf42adc5c0bdf2a
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-04-30 13:56:19 +0000

    Subindo customizam de listagem, menu, formulario para baixa de estoque

--- ax4b_inventory/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ ax4b_inventory/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "cptm_inventory",
+    'name': "AX4B Inventory",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,12 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
+        'views/baixa_estoque_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode

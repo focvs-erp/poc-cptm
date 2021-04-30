bcf72b445703fe41f5e895c40d7ff4ad0420d330
Anderson Peruci <anderson.peruci@ax4b.com>
2021-04-30 16:16:39 -0300

    Adição dos campos related com a tabela requisição de compras

--- ax4b_inventory/models/baixa_estoque.py
+++ ax4b_inventory/models/baixa_estoque.py
@@ -26,7 +26,7 @@ class Baixa_Estoque(models.Model):
 
     tabela_requisicao = fields.Many2one('x_requisicoes_de_compr', string = "Tabela Requisição")
     nome_da_requisicao_related = fields.Char(related='tabela_requisicao.x_studio_nome_da_requisio', string="Nome da requisição")
-    # produtos_da_requisicao_related = fields.One2many(related='tabela_requisicao.x_studio_one2many_field_jwTT5', string="Produto da requisição")  
+    produtos_da_requisicao_related = fields.One2many(related='tabela_requisicao.x_studio_one2many_field_jwTT5', string="Produto da requisição")  
         
 #   cd_produto = fields.One2Many(string= "Código do Produto")
 #    valor_total = fields.Float(string = "Valor Total", compute="_valor_total", store=True)

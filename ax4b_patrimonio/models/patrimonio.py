ab7864dd0869ea1a22f974b3245d03afb319fa8d
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-06 12:36:36 -0300

    Incluindo campos Many2one e related na aba orçamento na model patrimonio

--- ax4b_patrimonio/models/patrimonio.py
+++ ax4b_patrimonio/models/patrimonio.py
@@ -59,7 +59,7 @@ class Patrimonio(models.Model):
     cod_ccus_info_add = fields.Date(string='Início da Depreciação')
 
     #Orçamento
-    # Tabela_Dotacao_orcamento = fields.Many2one('x_dotacao', string = "Dotação Orçamentária")
+    Tabela_Dotacao_orcamento = fields.Many2one('x_dotacao', string = "Dotação Orçamentária")
     # nome_poder_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_many2one_field_4XDnU', string="Poder")
     # cod_poder_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_cdigo_do_poder', string="Poder")
     # nome_orgao_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_many2one_field_TbDWz', string="Poder")

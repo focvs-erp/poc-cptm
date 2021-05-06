576803bda5fe9b0ce14c020c9591a96e629d966f
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-06 16:54:40 -0300

    Incluindo campos Many2one e related na aba orçamento na model patrimonio

--- ax4b_patrimonio/models/patrimonio.py
+++ ax4b_patrimonio/models/patrimonio.py
@@ -66,10 +66,10 @@ class Patrimonio(models.Model):
     cod_orgao_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_cd_orgao', string="Código do Órgão")
     nome_unidade_orcamento = fields.Many2one(related='Tabela_Dotacao_orcamento.x_studio_ds_uo', string="Unidade Orçamentária")
     cod_unidade_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_ds_uo.x_studio_cdigo', string="Código da Unidade Orçamentária")
-    # nome_grupo_orcamento = fields.Many2one(related='Tabela_Dotacao_orcamento.x_studio_ds_grupo', string="Grupo")
-    # cod_grupo_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_ds_uo.x_studio_cd_grupo', string="Código do Grupo")
-    # nome_funcao_orcamento = fields.Many2one(related='Tabela_Dotacao_orcamento.x_studio_ds_funcao', string="Função")
-    # cod_funcao_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_ds_uo.x_studio_cd_funcao', string="Código da Função")
+    nome_grupo_orcamento = fields.Many2one(related='Tabela_Dotacao_orcamento.x_studio_ds_grupo', string="Grupo")
+    cod_grupo_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_ds_uo.x_studio_cd_grupo', string="Código do Grupo")
+    nome_funcao_orcamento = fields.Many2one(related='Tabela_Dotacao_orcamento.x_studio_ds_funcao', string="Função")
+    cod_funcao_orcamento = fields.Char(related='Tabela_Dotacao_orcamento.x_studio_ds_uo.x_studio_cd_funcao', string="Código da Função")
    
     
     # Campos já existentes dentro do asset, apenas usados para edição de nomes

470d80a060e19dce43234ed946808226ce089d01
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-07 17:57:14 -0300

    Colocando readonly no campo de código na criação de Patrimônio

--- ax4b_patrimonio/__manifest__.py
+++ ax4b_patrimonio/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ax4b_patrimonio",
+    'name': "AX4B Patrimônio",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as

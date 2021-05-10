bf8b6b294be9b579d0e59ee687fcf02837b59192
Matheus Solai Semaili <matheus.semaili@ax4b.com>
2021-05-10 18:33:39 -0300

    teste metodo Fornecedores usando api.onchange

--- ax4b_purchase/models/fornecedores_cotacao.py
+++ ax4b_purchase/models/fornecedores_cotacao.py
@@ -26,12 +26,12 @@ class FornecedoresDaCotacao(models.Model):
    #       obj.write({'contato': lista })
    #       return obj
 
-           @api.depends('fornecedores')
-          def Fornecedores_Contato(self):
-             for record in self:
-                 record.teste = 'ola'
-                  # for contato in record.contato:
-                  #    record.teste = record.contato.name
+         @api.onchange('fornecedores')
+         def Fornecedores_Contato(self):
+            for record in self:
+               record.teste = 'ola'
+                # for contato in record.contato:
+                #    record.teste = record.contato.name
             
 
 

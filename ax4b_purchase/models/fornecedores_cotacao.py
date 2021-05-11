05cde9aa35ed9244149f0f6ff044b07b27a3dd2f
Matheus Solai Semaili <matheus.semaili@ax4b.com>
2021-05-11 11:03:29 -0300

    teste metodo Fornecedores criando array no metodo _contato_fornecedores

--- ax4b_purchase/models/fornecedores_cotacao.py
+++ ax4b_purchase/models/fornecedores_cotacao.py
@@ -6,55 +6,45 @@ class FornecedoresDaCotacao(models.Model):
    _description = 'Fornecedores da Cotação'
     
    name = fields.Char() 
-   teste = fields.Char(compute='_contato_fornecedores', store= True)
+
    prioridade = fields.Selection([('0', 'Normal'), ('1', 'Urgent')], 'Priority', default='0', index=True)
    cotacao_de_compra = fields.Many2one("purchase.cotacao_compra", invisible=True, string="Cotação de Compra")
-   fornecedores = fields.Many2one("res.partner", string="fornecedores")
+   fornecedores = fields.Many2one("res.partner", string="Fornecedores")
    #contact = fields.Char(related="vendors.name")
-   contato = fields.One2many(related="fornecedores.child_ids", string="contato")
-   email = fields.Char(related="fornecedores.email", string="email")
-   telefone = fields.Char(related="fornecedores.phone", string="telefone") 
-   celular = fields.Char(related="fornecedores.mobile", string="celular") 
+   contato_fornecedores = fields.One2many(related="fornecedores.child_ids", string="Contato")
+   # escolha_de_contato = fields.Selection([('', 'record.contato_fornecedores.name'), ['4','Sem registros']],compute='_contato_fornecedores', store= True)
+   teste = fields.Selection(selection=lambda self: self._contato_fornecedores(), string="Contato", default="Sem registro")
+   # selecao_contato = fields.Char(compute='_selecao_contato', store=True)
+   # email_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
+   # telefone_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
+   # celular_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
+   email = fields.Char(related="fornecedores.email", string="Email")
+   telefone = fields.Char(related="fornecedores.phone", string="Telefone") 
+   celular = fields.Char(related="fornecedores.mobile", string="Celular") 
    #nome_fornecedor = fields.Char(related="fornecedores.name")
   
    
-   #   @api.model
-   #   def Contato(self, vals):
-   #      #for Lista in self:
-   #      obj = super(FornecedoresDaCotacao, self).create(vals)
-   #      lista = self.get('contato')
-   #       obj.write({'contato': lista })
-   #       return obj
-
    @api.depends('fornecedores')
    def _contato_fornecedores(self):
+       contato_array= []
       for record in self:
-         if record.contato:
-            for contato in record.contato:
-               record.teste = contato.name
+         if record.contato_fornecedores:
+            for contato in record.contato_fornecedores:
+               # if self.escolha_de_contato == 'contato':
+               #  value = "record.contato_fornecedores.name"
+               contato_array.append(contato.name)
 
 
-   #    @api.model
-   #    def Email(self, vals)
-   #       obj = super(FornecedoresDaCotacao, self).create(vals)
-   #       email = self.get('email')
-   #       obj.write({'email': email })
-   #       return obj
-
-   #   @api.model
-   #   def Telefone(self, vals)
-   #      obj = super(FornecedoresDaCotacao, self).create(vals)
-   #       telefone = self.get('telefone')
-   #       obj.write({'telefone': telefone  })
-   #       return obj
-
-   #   @api.model
-   #   def Celular(self, vals)
-   #      obj = super(FornecedoresDaCotacao, self).create(vals)
-   #      celular = self.get('celular')
-   #      obj.write({'celular': celular })
-   #       return obj
-
+   # @api.depends('contato_fornecedores')
+   # def _selecao_contato(self):
+   #    for record in self:
+   #       if record.contato_fornecedores:
+   #          for contato in record.contato_fornecedores:
+   #             if(contato.name == record.escolha_de_contato.value):
+   #                record.email_contato_fornecedores = record.contato_fornecedores.email
+   #                record.telefone_contato_fornecedores = record.contato_fornecedores.phone
+   #                record.celular_contato_fornecedores = record.contato_fornecedores.mobile
+                  
 
 
         

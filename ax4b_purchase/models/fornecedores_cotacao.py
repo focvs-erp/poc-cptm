74e63228a758b73c3f719cf112a731592c29ec25
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-11 18:37:26 -0300

    Testando super para obter nome do contato sem o print

--- ax4b_purchase/models/fornecedores_cotacao.py
+++ ax4b_purchase/models/fornecedores_cotacao.py
@@ -29,7 +29,7 @@ class FornecedoresDaCotacao(models.Model):
    def _add_contato(self):
       listEmail = []
       text = self.texto
-      listEmail.append(('1',str(text)))
+      listEmail.append('1',text)
       return listEmail
 
 

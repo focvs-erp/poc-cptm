42562d70ac2912bcfd62fa02f999661823962c5f
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-11 16:38:40 -0300

    Teste - Adicionando log

--- ax4b_purchase/models/fornecedores_cotacao.py
+++ ax4b_purchase/models/fornecedores_cotacao.py
@@ -15,7 +15,7 @@ class FornecedoresDaCotacao(models.Model):
    #contact = fields.Char(related="vendors.name")
    contato_fornecedores = fields.One2many(related="fornecedores.child_ids", string="Contato")
    # escolha_de_contato = fields.Selection([('', 'record.contato_fornecedores.name'), ['4','Sem registros']],compute='_contato_fornecedores', store= True)
-   teste = fields.Selection(selection='_add_contato', string="Contato Fornecedor")
+   teste = fields.Selection(compute='_add_contato', string="Contato Fornecedor")
    # selecao_contato = fields.Char(compute='_selecao_contato', store=True)
    # email_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
    # telefone_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)

7f3cf73c974013e6658aed311348a3ce3ccc8f79
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-12 18:00:39 -0300

    Subindo Boolean em Fornecedores

--- ax4b_accounting/models/fornecedor.py
+++ ax4b_accounting/models/fornecedor.py
@@ -6,5 +6,5 @@ from odoo import models, fields, api
 class fornecedor(models.Model):
     _inherit = 'res.partner'
 
-    active = fields.Boolean(string="Bloquer Cadastro", default=True)
+    active = fields.Boolean(string="Active", default=True)
 

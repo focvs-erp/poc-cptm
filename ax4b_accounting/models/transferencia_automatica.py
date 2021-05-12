ecff74e8d0c091a54fcaece5dccca6d17dda172d
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-12 12:26:34 -0300

    Fazendo correção em erro dentro da Transferência Automática

--- ax4b_accounting/models/transferencia_automatica.py
+++ ax4b_accounting/models/transferencia_automatica.py
@@ -7,5 +7,4 @@ class TransferenciaAutomatica(models.Model):
     _inherit = "account.transfer.model"
     
     date_stop = fields.Date(string="Data Validade")
-    percent = fields.Date(string="Percentual")
     centro_custo_related = fields.Many2one('x_centro_de_custo', string='Centro de Custo')
--- ax4b_accounting/models/transferencia_automatica_line.py
+++ ax4b_accounting/models/transferencia_automatica_line.py
@@ -6,5 +6,4 @@ from odoo import models, fields, api
 class TransferenciaAutomaticaLine(models.Model):
     _inherit = "account.transfer.model.line"
     
-    percent = fields.Date(string="Percentual")
     centro_custo_related = fields.Many2one('x_centro_de_custo', string='Centro de Custo')

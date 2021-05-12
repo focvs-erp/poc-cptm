926787a64a71e754b68df1845941b6e2c111177b
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-12 17:56:20 -0300

    ajuste combo

--- ax4b_purchase/models/fornecedores_cotacao.py
+++ ax4b_purchase/models/fornecedores_cotacao.py
@@ -7,72 +7,21 @@ class FornecedoresDaCotacao(models.Model):
    _description = 'Fornecedores da Cotação'
 
    name = fields.Char() 
+  
    prioridade = fields.Selection([('0', 'Normal'), ('1', 'Urgent')], 'Priority', default='0', index=True)
    cotacao_de_compra = fields.Many2one("purchase.cotacao_compra", invisible=True, string="Cotação de Compra")
    fornecedores = fields.Many2one("res.partner", string="Fornecedores")
-   nome_contato = fields.Char()
    contato_fornecedores = fields.Many2one("res.partner", string="Contato")
-  
-   # escolha_de_contato = fields.Selection([('', 'record.contato_fornecedores.name'), ['4','Sem registros']],compute='_contato_fornecedores', store= True)
-   #contact = fields.Char(related="vendors.name")
-   # telefone_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
-   # celular_contato_fornecedores = fields.Char(compute='_selecao_contato', store=True)
-   # selecao_contato = fields.Char(compute='_selecao_contato', store=True)
-   #nome_fornecedor = fields.Char(related="fornecedores.name") fornecedores.child_ids
 
-   teste = fields.Selection(contato_array, string="Contato Fornecedor")
-   email_contato_fornecedores = fields.Char(compute='_add_contato', store=True)
-   email = fields.Char(related="fornecedores.email", string="Email")
-   telefone = fields.Char(related="fornecedores.phone", string="Telefone") 
-   celular = fields.Char(related="fornecedores.mobile", string="Celular") 
+   email = fields.Char(related="contato_fornecedores.email", string="Email")
+   telefone = fields.Char(related="contato_fornecedores.phone", string="Telefone") 
+   celular = fields.Char(related="contato_fornecedores.mobile", string="Celular") 
   
-   
-   
    @api.onchange('fornecedores')
    def _onchange_fornecedore(self):
       for record in self:
          if record.fornecedores.id:
             return {'domain': {'contato_fornecedores': [('parent_id', '=', record.fornecedores.id)]}}
          else:
-            return {'domain': {'contato_fornecedores': []}}
-               
-
-   # @api.depends('fornecedores')
-   # def _add_contato(self):
-   #    for record in self:
-   #        if record.contato_fornecedores:
-   #           for contato in record.contato_fornecedores:
-   #             contato_array.append((str(contato.name), str(contato.name)))
-  
-
-   # @api.depends('fornecedores')
-   # def _selecao_contato(self):
-   #    for record in self:
-   #       if record.contato_fornecedores:
-   #          for contato in record.contato_fornecedores:
-   #             listEmail = [('1',"Rafael teste ")]
-   #             # if(contato.name == record.escolha_de_contato.value):
-   #             record.email_contato_fornecedores = "Rafael"
-   #             record.teste = listEmail
-   #             # record.telefone_contato_fornecedores = record.contato_fornecedores.phone
-   #             # record.celular_contato_fornecedores = record.contato_fornecedores.mobile
-                  
-
-   # @api.depends('fornecedores')
-   # def _add_contato(self):
-   #    list1 = []
-   #    list1.append(('1', 'option1'))
-   #    if self.contato_fornecedores:
-   #       list1.append((str('3', 'option32')))
-
-         # for contato in record.contato_fornecedores:
-            # list1.append((str(contato.name), str(contato.name)))
-      # self.teste = list1            
-      # return list1
-        
-   
-   # @api.depends('fornecedores')
-   # def _add_contato(self):
-   #    contato_array= []
-      
-   #    return [('1', 'option1'), ('2', 'option2')]
+            return {'domain': {'contato_fornecedores': []}}               
+            

eb3a110a2f2122f1bd3a4a8352bd024fc58e60b5
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-12 14:58:17 -0300

    Excluindo centro de custo dentro de Transferencia Automatica

--- ax4b_accounting/models/transferencia_automatica.py
+++ ax4b_accounting/models/transferencia_automatica.py
@@ -7,4 +7,3 @@ class TransferenciaAutomatica(models.Model):
     _inherit = "account.transfer.model"
     
     date_stop = fields.Date(string="Data Validade")
-    # centro_custo_related = fields.Many2one('x_centro_de_custo', string='Centro de Custo')

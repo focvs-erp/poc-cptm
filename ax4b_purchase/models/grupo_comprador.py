d94461d1c3ad9989250c78cc4616666738554086
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-13 14:15:29 -0300

    Testando a função ao botão enviar e-mail

--- ax4b_purchase/models/grupo_comprador.py
+++ ax4b_purchase/models/grupo_comprador.py
@@ -4,10 +4,8 @@ class GrupoComprador(models.Model):
    _name = 'purchase.grupo_comprador'
    _description = 'Grupo de Compradores'
 
-   # cd_grupo_comprador = fields.Char(string="Código Grupo Comprador")
    name = fields.Char(string="Código Grupo Comprador")
    ds_grupo_comprador = fields.Text(string="Descrição Grupo Comprador")
-#    cd_ativo = fields.Char(string="Ativo")
    cd_ativo = fields.Boolean(string='Ativo', default=True)
    compradores = fields.Many2many('purchase.comprador')
 

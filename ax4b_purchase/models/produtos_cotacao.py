926a0aa2ef32fdf547158b800bd4f75ced6336e2
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-13 12:38:03 -0300

    adicionado produtos da cotação

--- ax4b_purchase/models/produtos_cotacao.py
+++ ax4b_purchase/models/produtos_cotacao.py
@@ -6,6 +6,7 @@ class ProdutosDaCotacao(models.Model):
    _name = 'purchase.produtos_cotacao'
    _description = 'Produtos da Cotação'
 
+   name = fields.Char(string="Produto da cotação") 
    requisicao = fields.Many2one("purchase.x_requisicoes_de_compr", string="Requisição")
    produtos_requisicao = fields.Many2one("x_produto_requisicao", string="Contato")
   

8f14b05c71d0827dd990490197d47f119770d2c1
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-13 19:17:51 -0300

    ajuste função adicionar produto da requisi~ção

--- ax4b_purchase/models/produtos_cotacao.py
+++ ax4b_purchase/models/produtos_cotacao.py
@@ -7,18 +7,18 @@ class ProdutosDaCotacao(models.Model):
    _description = 'Produtos da Cotação'
 
    name = fields.Char(string="Produto da cotação") 
-   requisicao = fields.Many2one("purchase.x_requisicoes_de_compr", string="Requisição")
+   # requisicao = fields.Many2one("x_requisicoes_de_compr", string="Requisição")
    cotacao_de_compra = fields.Many2one("purchase.cotacao_compra", invisible=True, string="Cotação de Compra")
-   produtos_requisicao = fields.Many2one("x_produto_requisicao", string="Contato")
+   produtos_requisicao = fields.Many2one("x_produto_requisicao", string="Produto")
+
+   quantidade = fields.Char(related="produtos_requisicao.x_studio_quantidade", string="Quantidade")
+   unidademedida = fields.Char(related="produtos_requisicao.x_studio_unidade_de_medida", string="Unidade") 
+   situacao = fields.Char(related="produtos_requisicao.x_studio_situao", string="Situação") 
   
-   # email = fields.Char(related="contato_fornecedores.email", string="Email")
-   # telefone = fields.Char(related="contato_fornecedores.phone", string="Telefone") 
-   # celular = fields.Char(related="contato_fornecedores.mobile", string="Celular") 
-  
-   @api.onchange('requisicao')
-   def _onchange_requisicao(self):
-      for record in self:
-         if record.requisicao.id:
-            return {'domain': {'produtos_requisicao': [('x_studio_many2one_field_oMlx9', '=', record.requisicao.id)]}}
-         else:
-            return {'domain': {'produtos_requisicao': []}}               
+   # @api.onchange('requisicao')
+   # def _onchange_requisicao(self):
+   #    for record in self:
+   #       if record.requisicao.id:
+   #          return {'domain': {'produtos_requisicao': [('x_studio_many2one_field_oMlx9', '=', record.requisicao.id)]}}
+   #       else:
+   #          return {'domain': {'produtos_requisicao': []}}               

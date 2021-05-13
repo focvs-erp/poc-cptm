7e9806cc0bea3f7cf939a0a6c66ccf0d535acee8
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-13 14:17:55 -0300

    Testando a função ao botão enviar e-mail

--- ax4b_purchase/models/cotacao_compra.py
+++ ax4b_purchase/models/cotacao_compra.py
@@ -68,7 +68,6 @@ class CotacaoDeCompras(models.Model):
 
     def btn_enviar_email(self):
         self.ensure_one()
-<<<<<<< HEAD
         
         template_obj = self.env['mail.template'].sudo().search([('name','=','Teste E-mail')], limit=1)  
         
@@ -83,23 +82,6 @@ class CotacaoDeCompras(models.Model):
                 'recipient_ids': 'anderson.peruci@ax4b.com'
             }
             create_and_send_email = self.env['mail.mail'].create(mail_values).send() 
-=======
-        
-        template_obj = self.env['mail.template'].sudo().search([('name','=','Teste E-mail')], limit=1)
-        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        
-        body_html = '<p>Dear Anderson,<br />\
-            We request your feedback for the employee Anderson as part \
-            of the Performance Review. Click on the link below to submit your \
-            comments.</p><br /><p><a href=%s class="btn btn-danger">Employee Peer Feedback</a><br></p>'
-        
-        mail_values = {                        
-            'body_html': body_html,
-            'recipient_ids': 'anderson.peruci@ax4b.com'
-        }
-        
-        create_and_send_email = self.env['mail.mail'].create(mail_values).send() 
->>>>>>> d11c63c4f6d7885200b882fea66f26479d04eb56
         # for peers in self.peer_employee_ids:
         #     _url = ''+ base_url +'/peer_feedback/'+ str(self.id) +'/'
         #     if peers.user_id.partner_id.id:

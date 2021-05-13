54fe9b5d55c1634843bb3949722ebcdef5885e92
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-13 08:48:42 -0300

    Subindo regra para rembolso

--- ax4b_accounting/models/rembolso.py
+++ ax4b_accounting/models/rembolso.py
@@ -7,7 +7,7 @@ class fornecedor(models.Model):
     _inherit = 'account.move	'
 
     checkbox_bloqueio = fields.Boolean(related='partner_id.bloquear_cadastro', string='Bloqueio de Cadastro', invisible="1")
-    payment_reference = fields.Char(String="TESTE")
+    # payment_reference = fields.Char(String="TESTE")
 
     # @api.depends('checkbox_bloqueio')
     # def _total(self):

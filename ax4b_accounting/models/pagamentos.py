e9a902deeae3be51a590981e68a1e73b7914ed03
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-13 12:07:19 -0300

    Subindo regra para pagamento

--- ax4b_accounting/models/pagamentos.py
+++ ax4b_accounting/models/pagamentos.py
@@ -6,16 +6,16 @@ from odoo import models, fields, api
 class Pagamento(models.Model):
     _inherit = 'account.payment'
 
-    partner_id = fields.Many2one('partner_id', string="Fornecedor")
-    checkbox_bloqueio = fields.Boolean(related='partner_id.bloquear_cadastro', compute="_bloqueio")
-    ref = fields.Char(string="teste")
+    # partner_id = fields.Many2one('partner_id', string="Fornecedor")
+    # checkbox_bloqueio = fields.Boolean(related='partner_id.bloquear_cadastro', compute="_bloqueio")
+    # ref = fields.Char(string="teste")
 
-    @api.depends('checkbox_bloqueio')
-    def _bloqueio(self):
-        for record in self:
-            if record.checkbox_bloqueio == True:
-                record.ref = "TRUE"
-            else:
-                record.ref = "FALSE"
+    # @api.depends('checkbox_bloqueio')
+    # def _bloqueio(self):
+    #     for record in self:
+    #         if record.checkbox_bloqueio == True:
+    #             record.ref = "TRUE"
+    #         else:
+    #             record.ref = "FALSE"
 
 

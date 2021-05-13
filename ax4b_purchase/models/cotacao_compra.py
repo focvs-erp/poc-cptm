8fed479b77bb9e20502a12d39d23d002f35dfbe2
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-13 16:21:58 -0300

    erro campo cotacao_de_compra

--- ax4b_purchase/models/cotacao_compra.py
+++ ax4b_purchase/models/cotacao_compra.py
@@ -27,7 +27,7 @@ class CotacaoDeCompras(models.Model):
         ("PEDIDODECOMPRA", "PEDIDO DE COMPRA"),
     ], default = "SDC")
 
-    produtos_da_cotacao = fields.One2many("purchase.produtos_cotacao","cotacao_de_compra",string="Produtos")
+    produtos_da_cotacao = fields.One2many("purchase.produtos_cotacao","cotacao_de_compra_produto",string="Produtos")
     fornecedores_da_cotacao = fields.One2many("purchase.fornecedores_cotacao","cotacao_de_compra",string="Fornecedores da Cotação")
 
     nome_do_poder = fields.Char(related='nota_de_reserva.x_studio_ds_poder_reserva')

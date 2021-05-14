3c9cf0b154a2e281f61c0997976df7983ad4d869
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-14 15:56:44 -0300

    filtros status produto requisição

--- ax4b_purchase/models/produtos_cotacao.py
+++ ax4b_purchase/models/produtos_cotacao.py
@@ -9,7 +9,7 @@ class ProdutosDaCotacao(models.Model):
    name = fields.Char(string="Produto da cotação") 
    
    cotacao_de_compra = fields.Many2one("purchase.cotacao_compra", invisible=True, string="Cotação de Compra")
-   requisicao = fields.Many2one("x_requisicoes_de_compr", string="Requisição")
+   # requisicao = fields.Many2one("x_requisicoes_de_compr", string="Requisição")
    produtos_requisicao = fields.Many2one("x_produto_requisicao", string="Produto")
 
    quantidade = fields.Integer(related="produtos_requisicao.x_studio_quantidade", string="Quantidade")
@@ -17,10 +17,10 @@ class ProdutosDaCotacao(models.Model):
    situacao = fields.Selection(related="produtos_requisicao.x_studio_situao", string="Situação") 
    # requisicao = fields.Many2one(related="produtos_requisicao.x_studio_unidade_de_medida", string="Unidade") 
 
-   @api.onchange('requisicao')
-   def _onchange_requisicao(self):
-      for record in self:
-         if record.requisicao.id:
-            return {'domain': {'produtos_requisicao': [('x_studio_many2one_field_oMlx9', '=', record.requisicao.id)]}}
-         else:
-            return {'domain': {'produtos_requisicao': []}}               
+   # @api.onchange('requisicao')
+   # def _onchange_requisicao(self):
+   #    for record in self:
+   #       if record.requisicao.id:
+   #          return {'domain': {'produtos_requisicao': [('x_studio_many2one_field_oMlx9', '=', record.requisicao.id)]}}
+   #       else:
+   #          return {'domain': {'produtos_requisicao': []}}               

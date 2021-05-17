7ae13fbe74fa81159bb542284ccbbdd6d06513ac
Matheus Solai Semaili <matheus.semaili@ax4b.com>
2021-05-17 09:46:32 -0300

    arrumando identação

--- ax4b_accounting/models/ecd.py
+++ ax4b_accounting/models/ecd.py
@@ -14,12 +14,12 @@ class Ecd(models.Model):
     ate = fields.Date(string="Áte")
     inc_dem = fields.Boolean(string="Incluir Demonstrativel Contaveis")
     periodo = fields.Selection([('0', 'Mensal'), ('1', 'Anual')])
-    sit_emp = fields.Selection([('0', 'Divisão'), ('1', 'Fusão'), ('2', 'Corporação'), ('3', 'Fechamento'), ('4', 'Transformação')],     string="Situação da Empresa")
-    per_abe = fields.Selection([('0', 'Normal'), ('1', 'Abertura'), ('2', 'Divisão,Fusão ou Aquisição')], string="Período Fiscal de       Abertura")
-    versao = fields.Selection([('0', '2.00'), ('1', '3.00'),  ('2', '4.00'),  ('3', '5.00'),  ('4', '6.00'),  ('5', '7.00')],             string="Versão do Layout")
+    sit_emp = fields.Selection([('0', 'Divisão'), ('1', 'Fusão'), ('2', 'Corporação'), ('3', 'Fechamento'), ('4', 'Transformação')],string="Situação da Empresa")
+    per_abe = fields.Selection([('0', 'Normal'), ('1', 'Abertura'), ('2', 'Divisão,Fusão ou Aquisição')], string="Período Fiscal de Abertura")
+    versao = fields.Selection([('0', '2.00'), ('1', '3.00'),  ('2', '4.00'),  ('3', '5.00'),  ('4', '6.00'),  ('5', '7.00')],string="Versão do Layout")
     tp_arq = fields.Selection([('0', 'Oríginal'), ('1', 'Substítuto')], string="Tipo de Arquivo")
     dt_enc = fields.Date(string="Data de Encerramento Exercicio Social")
-    tp_inst = fields.Selection([('0', 'Pj em Geral'), ('1', 'Pj em Geral-Lucro Presumido'),  ('2', 'Financeiras'),  ('3',                 'Seguradoras'),  ('4', 'Imunes e Insentas em Geral'),  ('5', 'Imunes e Insentas-Financeiras'),  ('6', 'Imunes e Insentas-             Seguradoras'),  ('7', 'Entidade Fechadas de Previdencias Complementar'), ('8', 'Partídos Políticos'), ('9', 'Lucro Presumidos-Financeiras')], string="Tipo de Instituição" )
+    tp_inst = fields.Selection([('0', 'Pj em Geral'), ('1', 'Pj em Geral-Lucro Presumido'),  ('2', 'Financeiras'),  ('3','Seguradoras'),  ('4', 'Imunes e Insentas em Geral'),  ('5', 'Imunes e Insentas-Financeiras'),  ('6', 'Imunes e Insentas-             Seguradoras'),  ('7', 'Entidade Fechadas de Previdencias Complementar'), ('8', 'Partídos Políticos'), ('9', 'Lucro Presumidos-Financeiras')], string="Tipo de Instituição" )
     num_Livro = fields.Char(string="Número do Livro")
     
     def btn_ok(self):

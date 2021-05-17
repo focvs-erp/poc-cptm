891c5536dece9bc2ef96b5f5218c5baafc5741bb
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-17 15:32:32 -0300

    teste atualizar status lista

--- ax4b_purchase/models/cotacao_compra.py
+++ ax4b_purchase/models/cotacao_compra.py
@@ -61,6 +61,7 @@ class CotacaoDeCompras(models.Model):
 
     def write(self, vals):
         res = super(CotacaoDeCompras, self).write(vals)
+        res.write({'name': "Rafael"})
         # raise ValidationError(('You cannot assign the Main Pricelist as Other Pricelist in PriceList Item'))
         return res
 

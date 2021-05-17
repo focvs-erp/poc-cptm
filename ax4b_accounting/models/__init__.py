dad320d68171593591d80ef085f65fc3e962ef4c
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-05-17 16:47:27 -0300

    remoção do bloqueio de fornecedor

--- ax4b_accounting/models/__init__.py
+++ ax4b_accounting/models/__init__.py
@@ -9,5 +9,5 @@ from . import plano_conta
 from . import transferencia_automatica
 from . import transferencia_automatica_line
 from . import ecd
-from . import fornecedor
-from . import conta
+# from . import fornecedor
+# from . import conta

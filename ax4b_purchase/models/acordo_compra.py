3a39b8419e17fe2e8f3990b8fdee369a111b0386
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-05-18 12:56:38 -0300

    remoção de campos dependentes do account

--- ax4b_purchase/models/acordo_compra.py
+++ ax4b_purchase/models/acordo_compra.py
@@ -6,10 +6,10 @@ from odoo.exceptions import UserError, ValidationError
 class AcordoDeCompra(models.Model):
     _inherit = 'purchase.requisition'
 
-    situacao_bloqueio = fields.Selection(related='vendor_id.situacao_bloqueio', invisible=1)
+    # situacao_bloqueio = fields.Selection(related='vendor_id.situacao_bloqueio', invisible=1)
 
-    @api.onchange('vendor_id')
-    def _fornecedor_bloqueado(self):
-        for record in self:
-            if record.situacao_bloqueio == '2':
-                raise ValidationError("Fornecedor bloqueado para transações")
+    # @api.onchange('vendor_id')
+    # def _fornecedor_bloqueado(self):
+    #     for record in self:
+    #         if record.situacao_bloqueio == '2':
+    #             raise ValidationError("Fornecedor bloqueado para transações")

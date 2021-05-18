5700d478f890ff967a12e5ac74d6404b8053ddee
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-05-18 13:05:45 -0300

    remoção do campo situação de bloqueio do account

--- ax4b_purchase/models/pedido_compra.py
+++ ax4b_purchase/models/pedido_compra.py
@@ -6,10 +6,10 @@ from odoo.exceptions import UserError, ValidationError
 class PedidoCompra(models.Model):
     _inherit = 'purchase.order'
 
-    situacao_bloqueio = fields.Selection(related='partner_id.situacao_bloqueio', invisible=1)
+    # situacao_bloqueio = fields.Selection(related='partner_id.situacao_bloqueio', invisible=1)
 
-    @api.onchange('partner_id')
-    def _fornecedor_bloqueado(self):
-        for record in self:
-            if record.situacao_bloqueio == '2':
-                raise ValidationError("Fornecedor bloqueado para transações")
+    # @api.onchange('partner_id')
+    # def _fornecedor_bloqueado(self):
+    #     for record in self:
+    #         if record.situacao_bloqueio == '2':
+    #             raise ValidationError("Fornecedor bloqueado para transações")

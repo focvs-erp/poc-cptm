435b3b1fca47da4a51eb1bc2cb0f1cd375a11003
Rafael Maico <rafael.oliveira@ax4b.com>
2021-05-19 17:01:21 -0300

    adicionado fornecedor selecionado campos calculados

--- ax4b_purchase/models/cotacao_compra.py
+++ ax4b_purchase/models/cotacao_compra.py
@@ -78,7 +78,7 @@ class CotacaoDeCompras(models.Model):
                 for produto in record.produtos_da_cotacao:
                     # codigo do produto produtos_requisicao
                     self._cr.execute('INSERT INTO fornecedores_selecionado_cotacao (cotacao_de_compra, fornecedores, produtos_cotacao) VALUES (%s,%s,%s)', (record.id, fornecedor.id,produto.produtos_requisicao.id))                                  
-    
+                    raise ValidationError("executado com sucesso")
     # @ api.model 
     # def fields_view_get (self, view_id = None, view_type = 'form', toolbar = False, submenu = False): 
     #     res = super ("purchase.cotacao_compra", self) .fields_view_get (view_id = view_id, 

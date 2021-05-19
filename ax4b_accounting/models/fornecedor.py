b735ec1738ed9737ece7fb1ace7f0eb325f40802
GabrielGomes125 <gabriel_gomesbatista@hotmail.com>
2021-05-19 16:02:14 -0300

    Subindo Fornecedor novamente

--- ax4b_accounting/models/fornecedor.py
+++ ax4b_accounting/models/fornecedor.py
@@ -7,8 +7,8 @@ from odoo.exceptions import UserError, ValidationError
 class Fornecedor(models.Model):
     _inherit = 'res.partner'
 
-    juros = fields.Many2one('account.juros', string="Código Juros")
-    multa = fields.Many2one('account.multa', string="Código Multa")
+    # juros = fields.Many2one('account.juros', string="Código Juros")
+    # multa = fields.Many2one('account.multa', string="Código Multa")
     situacao_bloqueio = fields.Selection([('1', 'Ativo'), ('2', 'Bloqueado')], required=False, default='1', readonly=True)
 
     def btn_desbloquear(self):

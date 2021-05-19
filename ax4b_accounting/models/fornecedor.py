d8533924ae264e7e124dd0030895105f631a40ad
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-05-19 16:23:18 -0300

    Subindo fornecedor

--- ax4b_accounting/models/fornecedor.py
+++ ax4b_accounting/models/fornecedor.py
@@ -8,7 +8,7 @@ class Fornecedor(models.Model):
     _inherit = 'res.partner'
 
     # juros = fields.Many2one('account.juros', string="Código Juros")
-    
+    # multa = fields.Many2one('account.multa', string="Código Multa")
     situacao_bloqueio = fields.Selection([('1', 'Ativo'), ('2', 'Bloqueado')], required=False, default='1', readonly=True)
 
     def btn_desbloquear(self):
@@ -19,7 +19,7 @@ class Fornecedor(models.Model):
         for record in self:
             record.situacao_bloqueio = '2'
 
-    multa = fields.Many2one('account.multa', string="Código Multa")
+    
         
         
 #         message_id = self.env['res.fornecedor_bloqueado_wizard'].create({'message': 'Osam gerados com Sucesso!'})

02fea410b799ba718c2adb8d9cbc5517476125ff
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-19 09:47:37 -0300

    Corrigindo a existência do campo currency_id_societaria

--- ax4b_accounting/models/patrimonio.py
+++ ax4b_accounting/models/patrimonio.py
@@ -44,9 +44,9 @@ class Patrimonio(models.Model):
     desc_obs_dados_garantia = fields.Char(string='Observações')
 
     # Depreciação Societária
-    depreciation_move_ids_societaria = fields.One2many('account.move', 'asset_id', string='Depreciation Lines', readonly=True, states={'draft': [('readonly', False)], 'open': [('readonly', False)], 'paused': [('readonly', False)]})
-    currency_id_societaria = fields.Many2one('res.currency', string='Currency', required=True, readonly=True, states={'draft': [('readonly', False)]},
-                                  default=lambda self: self.env.company.currency_id.id)
+    # depreciation_move_ids_societaria = fields.One2many('account.move', 'asset_id', string='Depreciation Lines', readonly=True, states={'draft': [('readonly', False)], 'open': [('readonly', False)], 'paused': [('readonly', False)]})
+    # currency_id_societaria = fields.Many2one('res.currency', string='Currency', required=True, readonly=True, states={'draft': [('readonly', False)]},
+    #                               default=lambda self: self.env.company.currency_id.id)
     
     # Informações adicionais
     qtd_info_add = fields.Integer(string='Quantidade')

59b8d988d03742982f538ed018ed5b256dd2e4fd
guilherme.santos <guilherme.santos@ax4b.com>
2021-05-20 17:22:07 -0300

    teste cálculo

--- ax4b_accounting/models/patrimonio.py
+++ ax4b_accounting/models/patrimonio.py
@@ -122,33 +122,33 @@ class Patrimonio(models.Model):
         self.write({'state': 'draft'})
 
     
-    # def compute_depreciation_board_societaria(self):
-    #     self.ensure_one()
-    #     amount_change_ids = self.depreciation_move_ids.filtered(lambda x: x.asset_value_change and not x.reversal_move_id).sorted(key=lambda l: l.date)
-    #     posted_depreciation_move_ids = self.depreciation_move_ids.filtered(lambda x: x.state == 'posted' and not x.asset_value_change and not x.reversal_move_id).sorted(key=lambda l: l.date)
-    #     already_depreciated_amount = sum([m.amount_total for m in posted_depreciation_move_ids])
-    #     depreciation_number = self.method_number
-    #     if self.prorata:
-    #         depreciation_number += 1
-    #     starting_sequence = 0
-    #     amount_to_depreciate = self.value_residual + sum([m.amount_total for m in amount_change_ids])
-    #     depreciation_date = self.first_depreciation_date
-    #     # if we already have some previous validated entries, starting date is last entry + method period
-    #     if posted_depreciation_move_ids and posted_depreciation_move_ids[-1].date:
-    #         last_depreciation_date = fields.Date.from_string(posted_depreciation_move_ids[-1].date)
-    #         if last_depreciation_date > depreciation_date:  # in case we unpause the asset
-    #             depreciation_date = last_depreciation_date + relativedelta(months=+int(self.method_period))
-    #     commands = [(2, line_id.id, False) for line_id in self.depreciation_move_ids.filtered(lambda x: x.state == 'draft')]
-    #     newlines = self._recompute_board(depreciation_number, starting_sequence, amount_to_depreciate, depreciation_date, already_depreciated_amount, amount_change_ids)
-    #     newline_vals_list = []
-    #     for newline_vals in newlines:
-    #         # no need of amount field, as it is computed and we don't want to trigger its inverse function
-    #         del(newline_vals['amount_total'])
-    #         newline_vals_list.append(newline_vals)
-    #     new_moves = self.env['account.move'].create(newline_vals_list)
-    #     for move in new_moves:
-    #         commands.append((4, move.id))
-    #     return self.write({'depreciation_move_ids': commands})
+    def compute_depreciation_board_societaria(self):
+        self.ensure_one()
+        amount_change_ids = self.depreciation_move_ids.filtered(lambda x: x.asset_value_change and not x.reversal_move_id).sorted(key=lambda l: l.date)
+        posted_depreciation_move_ids = self.depreciation_move_ids.filtered(lambda x: x.state == 'posted' and not x.asset_value_change and not x.reversal_move_id).sorted(key=lambda l: l.date)
+        already_depreciated_amount = sum([m.amount_total for m in posted_depreciation_move_ids])
+        depreciation_number = self.method_number
+        if self.prorata:
+            depreciation_number += 1
+        starting_sequence = 0
+        amount_to_depreciate = self.value_residual + sum([m.amount_total for m in amount_change_ids])
+        depreciation_date = self.first_depreciation_date
+        # if we already have some previous validated entries, starting date is last entry + method period
+        if posted_depreciation_move_ids and posted_depreciation_move_ids[-1].date:
+            last_depreciation_date = fields.Date.from_string(posted_depreciation_move_ids[-1].date)
+            if last_depreciation_date > depreciation_date:  # in case we unpause the asset
+                depreciation_date = last_depreciation_date + relativedelta(months=+int(self.method_period))
+        commands = [(2, line_id.id, False) for line_id in self.depreciation_move_ids.filtered(lambda x: x.state == 'draft')]
+        newlines = self._recompute_board(depreciation_number, starting_sequence, amount_to_depreciate, depreciation_date, already_depreciated_amount, amount_change_ids)
+        newline_vals_list = []
+        for newline_vals in newlines:
+            # no need of amount field, as it is computed and we don't want to trigger its inverse function
+            del(newline_vals['amount_total'])
+            newline_vals_list.append(newline_vals)
+        new_moves = self.env['account.move'].create(newline_vals_list)
+        for move in new_moves:
+            commands.append((4, move.id))
+        return self.write({'depreciation_move_ids': commands})
 
     # @api.onchange('name')
     # def set_code(self):

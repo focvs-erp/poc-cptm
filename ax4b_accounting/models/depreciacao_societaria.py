11d5dc350a42847508161162e895f2e046351f8d
guilherme.santos <guilherme.santos@ax4b.com>
2021-05-20 11:51:40 -0300

    deprecicao societaria

--- ax4b_accounting/models/depreciacao_societaria.py
+++ ax4b_accounting/models/depreciacao_societaria.py
@@ -6,7 +6,7 @@ class DepreciacaoSocietaria(models.Model):
 
     # asset_id_societaria = fields.Many2one('account.asset', string='Asset', index=True, ondelete='cascade', copy=False, domain="[('company_id', '=', company_id)]")
     asset_id_societaria = fields.Many2one('account.asset', string='Asset')
-    asset_asset_type_societaria = fields.Selection(related='asset_id.asset_type')
-    asset_remaining_value_societaria = fields.Monetary(string='Depreciable Value', copy=False)
-    asset_depreciated_value_societaria = fields.Monetary(string='Cumulative Drepreciation', copy=False)
-    asset_value_change_societaria = fields.Boolean(help='This is a technical field set to true when this move is the result of the changing of value of an asset')
+    # asset_asset_type_societaria = fields.Selection(related='asset_id.asset_type')
+    # asset_remaining_value_societaria = fields.Monetary(string='Depreciable Value', copy=False)
+    # asset_depreciated_value_societaria = fields.Monetary(string='Cumulative Drepreciation', copy=False)
+    # asset_value_change_societaria = fields.Boolean(help='This is a technical field set to true when this move is the result of the changing of value of an asset')

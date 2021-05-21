214bc2f4b4e86080963e609dacacf43ab5be8560
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-05-21 09:53:03 -0300

    inclusão de propriedades na model depreciação societária, inclusão de campo nos na view do form patrimônio

--- ax4b_accounting/models/depreciacao_societaria.py
+++ ax4b_accounting/models/depreciacao_societaria.py
@@ -1,23 +1,17 @@
 from odoo import models, fields, api
 
+
 class DepreciacaoSocietaria(models.Model):
     _name = 'account.depreciacao_societaria'
     _description = 'Depreciação Societária'
 
-    currency_id = fields.Many2one('res.currency', string='Currency', required=True, readonly=True, states={'draft': [('readonly', False)]},
-    default=lambda self: self.env.company.currency_id.id)
-    asset_id_societaria = fields.Many2one('account.asset', string='Asset')
-    asset_asset_type_societaria = fields.Selection(related='asset_id_societaria.asset_type')
+    name = fields.Char(string='Entrada de Diário', copy=False)
+
+    asset = fields.Many2one('account.asset', string='Asset', invisible=True)
+    currency_id = fields.Many2one(related='asset.currency_id', invisible=True)
+
+    ref_societaria = fields.Char(string="Referência", copy=False)
+    date_societaria = fields.Date(string='Data de Depreciação', copy=False)
+    amount_total_societaria = fields.Monetary(string='Depreciação', copy=False)
+    asset_depreciated_value_societaria = fields.Monetary(string='Drepreciação Cumulativa', copy=False)
     asset_remaining_value_societaria = fields.Monetary(string='Depreciable Value', copy=False)
-    # asset_depreciated_value_societaria = fields.Monetary(string='Cumulative Drepreciation', copy=False)
-    asset_manually_modified_societaria = fields.Boolean(help='This is a technical field stating that a depreciation line has been manually modified. It is used to recompute the depreciation table of an asset/deferred revenue.', copy=False)
-    asset_value_change_societaria = fields.Boolean(help='This is a technical field set to true when this move is the result of the changing of value of an asset')
-
-    asset_ids_societaria = fields.One2many('account.asset', string='Assets', compute="_compute_asset_ids")
-    asset_ids_display_name_societaria = fields.Char(compute="_compute_asset_ids")  # just a button label. That's to avoid a plethora of different buttons defined in xml
-    asset_id_display_name_societaria = fields.Char(compute="_compute_asset_ids")   # just a button label. That's to avoid a plethora of different buttons defined in xml
-    number_asset_ids_societaria = fields.Integer(compute="_compute_asset_ids")
-    draft_asset_ids_societaria = fields.Boolean(compute="_compute_asset_ids")
-
-    # reversed_entry_id is set on the reversal move but there is no way of knowing is a move has been reversed without this
-    reversal_move_id_societaria = fields.One2many('account.move', 'reversed_entry_id')

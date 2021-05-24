936ad442e60984f6570b3e1d5c524f58bf533289
Anderson Peruci <anderson.peruci@ax4b.com>
2021-05-24 12:51:48 -0300

    Cálculo automático para data de início de depreciação

--- ax4b_accounting/models/patrimonio.py
+++ ax4b_accounting/models/patrimonio.py
@@ -64,7 +64,7 @@ class Patrimonio(models.Model):
     metodo_depreciado_info_add = fields.Float(string='Fator de Declínio', default=0.30)
     cod_forn_info_add = fields.Boolean()
     cod_forn_date_info_add = fields.Date(string='Prorata Date', default=lambda self: fields.Date.today())
-    cod_ccus_info_add = fields.Date(string='Início da Depreciação') 
+    cod_ccus_info_add = fields.Date(string='Início da Depreciação', compute='_compute_first_depreciation_date_societaria',) 
     method_number_info_add = fields.Integer(string='', default = 5)
     method_period_info_add = fields.Selection([('1', 'Month'),('12', 'Year')],'Type', default='1')
     
@@ -241,3 +241,16 @@ class Patrimonio(models.Model):
                 else:
                     amount = linear_amount
         return amount
+
+    @api.depends('acquisition_date', 'original_move_line_ids', 'metodo_info_add', 'company_id')
+    def _compute_first_depreciation_date_societaria(self):
+        for record in self:
+            pre_depreciation_date = record.acquisition_date or min(record.original_move_line_ids.mapped('date'), default=record.acquisition_date) or fields.Date.today()
+            depreciation_date = pre_depreciation_date + relativedelta(day=31)
+            # ... or fiscalyear depending the number of period
+            if record.method_period == '12':
+                depreciation_date = depreciation_date + relativedelta(month=int(record.company_id.fiscalyear_last_month))
+                depreciation_date = depreciation_date + relativedelta(day=record.company_id.fiscalyear_last_day)
+                if depreciation_date < pre_depreciation_date:
+                    depreciation_date = depreciation_date + relativedelta(years=1)
+            record.first_depreciation_date = depreciation_date
